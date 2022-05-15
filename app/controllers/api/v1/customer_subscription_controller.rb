87ffb2c7091ced948e97bf2a7f95094e29e3c214
TJ Williams <williamsjr.timothy1@gmail.com>
2022-05-15 10:11:10 -0400

    Add: Rescue to application controller

--- app/controllers/api/v1/customer_subscription_controller.rb
+++ app/controllers/api/v1/customer_subscription_controller.rb
@@ -1,14 +1,10 @@
 class Api::V1::CustomerSubscriptionController < ApplicationController 
-  def create 
-    customer = Customer.find_by(email: params[:customer])
-    subscription = Subscription.find_by(title: params[:subscription])
-    tea = Tea.find_by(title: params[:tea])
+  def create
+    customer = Customer.find_by!(email: params[:customer])
+    subscription = Subscription.find_by!(title: params[:subscription])
+    tea = Tea.find_by!(title: params[:tea])
 
-    if customer.present?
-      SubscriptionTea.create(subscription_id: subscription.id, tea_id: tea.id)
-      render json: CustomerSubscription.create(customer_id: customer.id, subscription_id: subscription.id)
-    else 
-      render json: MessageSerializer.hashed_error
-    end 
+    SubscriptionTea.create(subscription_id: subscription.id, tea_id: tea.id)
+    render json: CustomerSubscription.create(customer_id: customer.id, subscription_id: subscription.id)
   end 
 end 
