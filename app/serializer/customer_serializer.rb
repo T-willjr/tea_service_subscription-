538dfa1c5832bc8b28f467d9bd3462bee76f2900
TJ Williams <williamsjr.timothy1@gmail.com>
2022-05-16 09:32:25 -0400

    Add/Update: Serializers

--- app/serializer/customer_serializer.rb
+++ app/serializer/customer_serializer.rb
@@ -1,7 +1,7 @@
 class CustomerSerializer
   include JSONAPI::Serializer
 
-  has_one :customer_subscription
-  has_one :subscription, through: :customer_subscription
-  has_many :teas, through: :subscription
+  has_many :customer_subscriptions
+  has_many :subscriptions, through: :customer_subscriptions
+  has_many :teas, through: :subscriptions
 end
--- app/serializer/subscription_serializer.rb
+++ app/serializer/subscription_serializer.rb
@@ -1,7 +1,8 @@
 class SubscriptionSerializer
   include JSONAPI::Serializer
 
-  has_many :customer_subscription
+  has_many :customer_subscriptions
+  has_many :customers, through: :customer_subscriptions
   has_many :subscription_teas
   has_many :teas, through: :subscription_teas
 end
