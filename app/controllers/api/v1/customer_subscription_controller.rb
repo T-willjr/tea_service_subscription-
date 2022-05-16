f73dfae7186556c7d25eebc90a09efed8ba123cb
TJ Williams <williamsjr.timothy1@gmail.com>
2022-05-16 08:43:46 -0400

    Add: Routes & Controller

--- app/controllers/api/v1/customer_subscription_controller.rb
+++ app/controllers/api/v1/customer_subscription_controller.rb
@@ -3,8 +3,13 @@ class Api::V1::CustomerSubscriptionController < ApplicationController
     customer = Customer.find_by!(email: params[:customer])
     subscription = Subscription.find_by!(title: params[:subscription])
     tea = Tea.find_by!(title: params[:tea])
-
     SubscriptionTea.create(subscription_id: subscription.id, tea_id: tea.id)
     render json: CustomerSubscription.create(customer_id: customer.id, subscription_id: subscription.id)
+  end 
+
+  def update 
+    customer = Customer.find_by!(email: params[:customer])
+    customer.cancel_subscription(params[:subscription_plan])
+    render json: MessageSerializer.hashed_subscription_cancelled(params[:subscription_plan])
   end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do  
+      patch "/cancel", to: "customer_subscription#update"
       resources :subscribe, only: [:create], controller: :customer_subscription
     end 
   end 
